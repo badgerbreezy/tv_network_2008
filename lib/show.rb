37ad7a3e417cb7471b9920f9f850d8561f7700ea
Brian Liu <brian.b.liu@gmail.com>
2020-09-01 10:16:02 -0600

    Create show class and test.

---
+++ lib/show.rb
@@ -0,0 +1,33 @@
+class Show
+  attr_reader :name, :creator, :characters, :actors
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+    @actors = show_actors
+  end
+
+
+  def total_salary
+    combined_salary = @characters.map do |character|
+      character.salary
+    end
+    return combined_salary.sum
+  end
+
+  def highest_paid_actor
+    highest_paid = @characters.sort_by do |character|
+      character.salary
+    end
+    return highest_paid.reverse[0].actor
+  end
+
+  def show_actors
+    actor_list = []
+    @characters.each do |character|
+      actor_list << character.actor
+    end
+    return actor_list
+  end
+
+end
